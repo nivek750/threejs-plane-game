54889acf68162c3abb4e9f0b02a645f9ffd2c9e1
Kevin Ha <nivek750@gmail.com>
2023-10-06 21:59:10 +1100

    add images

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const textureLoader = new THREE.TextureLoader();
+
+const texture = textureLoader.load('assets/Ships/ship_0000.png');
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -12,6 +15,12 @@ const geometry = new THREE.SphereGeometry( 1, 16, 16 );
 const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
 const sphere = new THREE.Mesh( geometry, material );
 scene.add( sphere );
+
+const geometry2 = new THREE.BoxGeometry();
+texture.magFilter = THREE.NearestFilter;
+const material2 = new THREE.MeshBasicMaterial({ map: texture, transparent: true });
+const mesh = new THREE.Mesh(geometry2, material2);
+scene.add(mesh);
 
 const gravity = 0.1;
 let velocity = 0;
@@ -32,6 +41,7 @@ function animate() {
     renderer.render( scene, camera );
 }
 
+//make ball jump
 window.addEventListener('keydown', (event) => {
     if (event.code === 'Space') {
         velocity = 2;
