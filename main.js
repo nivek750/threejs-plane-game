feaab4ea21e60cf0aeb98e1d6c3482bc97693ac9
Kevin Ha <nivek750@gmail.com>
2023-10-07 17:00:20 +1100

    add some bullet firing other checks

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const textureLoader = new THREE.TextureLoader();
 
 const texture = textureLoader.load('assets/Ships/ship_0000.png');
+const bulletTexture = textureLoader.load('assets/Tiles/tile_0000.png');
 const position = new THREE.Vector3();
 const targetPosition = new THREE.Vector3();
 
@@ -19,20 +20,25 @@ const material2 = new THREE.MeshBasicMaterial({ map: texture, transparent: true 
 const planeMesh = new THREE.Mesh(geometry2, material2);
 scene.add(planeMesh);
 
-const gravity = 0.1;
-let velocity = 0;
+const bulletGeometry = new THREE.PlaneGeometry();
+const bulletMaterial = new THREE.MeshBasicMaterial({ map: bulletTexture, transparent: true });
+
+const bulletThreshold = 10;
+let lastBulletTime = 0;
 
 const keys = {
     w: false,
     a: false,
     s: false,
-    d: false
+    d: false,
+    space: false,
 };
 
 camera.position.z = 10;
 
 function animate() {
     requestAnimationFrame( animate );
+    const currentTime = Date.now();
 
     if(keys.w) {
         targetPosition.y += 0.2;
@@ -50,11 +56,35 @@ function animate() {
         targetPosition.x += 0.2;
     }
 
+    // Clamp the target position to the bounds of the screen
+    targetPosition.x = THREE.MathUtils.clamp(targetPosition.x, -10, 10);
+    targetPosition.y = THREE.MathUtils.clamp(targetPosition.y, -5, 5);
+
+    if(keys.space && currentTime - lastBulletTime > 200) {
+        const bulletMesh = new THREE.Mesh(bulletGeometry, bulletMaterial);
+        bulletMesh.position.copy(planeMesh.position);
+        bulletMesh.position.y += 0.5;
+        scene.add(bulletMesh);
+        lastBulletTime = currentTime;
+    }
+
     // Lerp the position towards the target position
     position.lerp(targetPosition, 0.1);
 
     // Update the plane's position
     planeMesh.position.copy( position );
+
+    // Update the position of the bullet meshes
+    scene.children.forEach(child => {
+        if (child instanceof THREE.Mesh && child.material === bulletMaterial) {
+            child.position.y += 0.5;
+
+            // Check if the bullet is out of bounds
+            if (child.position.y > bulletThreshold) {
+                scene.remove(child);
+            }
+        }
+    });
 
     renderer.render( scene, camera );
 }
@@ -62,20 +92,19 @@ function animate() {
 document.addEventListener('keydown', event => {
     switch (event.key) {
         case 'w':
-            // targetPosition.y += 0.2;
             keys.w = true;
             break;
         case 'a':
-            // targetPosition.x -= 0.2;
             keys.a = true;
             break;
         case 's':
-            // targetPosition.y -= 0.2;
             keys.s = true;
             break;
         case 'd':
-            // targetPosition.x += 0.2;
             keys.d = true;
+            break;
+        case ' ':
+            keys.space = true;
             break;
     }
 
@@ -92,6 +121,9 @@ document.addEventListener('keydown', event => {
                 break;
             case 'd':
                 keys.d = false;
+                break;
+            case ' ':
+                keys.space = false;
                 break;
         }
     });
